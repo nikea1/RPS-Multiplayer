f5195849081eec430e8a098f39bc35c4804617fc
Adenike Adeyemi <adenikeadeyemi@Adenike-Adeyemis-MacBook-Pro.local>
2016-08-15 04:12:44 -0400

    changed method of setting to firebase and retriving from firebase

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -11,94 +11,36 @@ $(document).ready(function(){
 	storageBucket: "",
 	};
 	firebase.initializeApp(config);
-	
+
+	var playerCount = 0;
 	var database = firebase.database();
-	var connectedRef = database.ref('.info/connected');
-	var connectionsRef = database.ref('/connections');
 	var p1choice = "";
 	var p2choice = "";
 	var p1Stats = [0,0]; //win, lose
 	var p2Stats = [0,0];
 	var rps = ['rock', 'paper', 'scissors'];
-	var player1Exists = false;
-	var player2Exists = false;
-	var currentPlayer = 0;
-  	var playerCount = 0;
-  	var playerKey = null;
-  	var gameOn = false;
-  	//creating xor function
+  	
   	function XOR(a,b){
   		return (a && !b) || (b && !a)
   	}
 
-
-  	//check player values---------------------------------------------------------------------------------------
-	database.ref('player').on("value", function(snapShot){
+	database.ref().on("value", function(snapShot){
 		
 		console.log(snapShot.val());
-
-		player1Exists = snapShot.child('1').exists();//checks player 1's existance
-		player2Exists = snapShot.child('2').exists();//checks player 2's existance
-
-
-		if(player1Exists){
-			console.log(snapShot.child('1').val().name);
-			$('.P1Name').text(snapShot.child('1').val().name);
-			$('#P1-Score').text('Wins: '+snapShot.child('1').val().win+' Loses: '+snapShot.child('1').val().lose);
-		}
-		else{
-			$('.P1Name').text('Waiting for Player 1');
-			$('#P1-Score').empty();
-		}
-
-
-
-		if(player2Exists){
-			console.log(snapShot.child('1').val().name);
-			$('.P2Name').text(snapShot.child('2').val().name);
-			$('#P2-Score').text('Wins: '+snapShot.child('2').val().win+' Loses: '+snapShot.child('2').val().lose);
-		}
-		else{
-			$('.P2Name').text('Waiting for Player 2');
-			$('#P2-Score').empty();
-		}
-
-		//keeps track of clients player number to set
-		if(player1Exists && !player2Exists){
-			playerCount = 2;
-			
-		}
-		else if(!player1Exists){
-			playerCount = 1;
-			
-		}
-
-		if(currentPlayer != 0 && !gameOn && player2Exists && player1Exists) setGame()
-
-		//deletes turn count when both players are not available at the same time
-  		if(!(player1Exists && player2Exists)){
-  			database.ref('turns').set(null);
-  		}
-
-  		console.log(currentPlayer);
-
-  		//allows watchers to join when seat is open
-		if(currentPlayer == 0 && !(player1Exists && player2Exists)) 
-			$('#playerInput').show();
-		
-		//---------------------------
-		
-
-	}, 
-	function(errorObj){console.log(errorObj.code)});
-	//--------------------------------------------------------------------------------
-
-
-	//set player
-	$('#nameSend').on('click', function(){
 		console.log(playerCount);
-		currentPlayer = playerCount;
-		console.log(currentPlayer);
+
+		if(snapShot.child('player/1').exists())
+			playerCount = 2;
+		else
+			playerCount = 1;
+
+		if(!snapShot.child('player/1').exists() ||  XOR(snapShot.child('player/2').exists(),  snapShot.child('player/1').exists())) $('#playerInput').show();
+		console.log(playerCount);
+
+	}, function(errorObj){console.log(errorObj.code)})
+
+	$('#nameSend').on('click', function(){
+
 		var playerName = $('#playerName').val().trim();
 
 		var players = database.ref().child('player')
@@ -113,18 +55,7 @@ $(document).ready(function(){
 			lose: 0
 
 		});
-
-		if(player1Exists && player2Exists){ 
-			database.ref('turns').set(1);
-			setGame();
-
-	}
-		
 		$('#playerInput').hide();
-		
-
-
-
 
 	});
 
@@ -134,9 +65,8 @@ $(document).ready(function(){
 	$('#chatSend').on('click', function(){
 
 		var c = $('#chatInput').val().trim();
-		var chat = database.ref().child('chat').push(c);
-		$('#chatInput').val('');
-		//$('#chatLog').text(c)
+
+		$('#chatLog').text(c)
 
 		return false;
 	});
@@ -178,8 +108,8 @@ $(document).ready(function(){
 
 		}
 		
-		//$('#P1-Score')
-		//	.html('Wins: <span class="P1Wins">'+p1Stats[0]+'</span> Loses: <span class="P1Loses">'+p1Stats[1]+'</span>')
+		$('#P1-Score')
+			.html('Wins: <span class="P1Wins">'+p1Stats[0]+'</span> Loses: <span class="P1Loses">'+p1Stats[1]+'</span>')
 			
 		
 	}
@@ -197,20 +127,13 @@ $(document).ready(function(){
 				.appendTo($('#player1'));*/
 
 		}
-		//$('#P2-Score')
-		//	.html('Wins: <span class="P2Wins">'+p2Stats[0]+'</span> Loses: <span class="P2Loses">'+p2Stats[1]+'</span>')
+		$('#P2-Score')
+			.html('Wins: <span class="P2Wins">'+p2Stats[0]+'</span> Loses: <span class="P2Loses">'+p2Stats[1]+'</span>')
 		
 	}
 
-	function setGame(){
-
-		if(currentPlayer != 0){
-
-			(currentPlayer == 1) ? setP1Game() : setP2Game();
-		}
-	}
-    //setP1Game();
-    //setP2Game();
+    setP1Game();
+    setP2Game();
     
     //run the comparison game
     function playGame(){

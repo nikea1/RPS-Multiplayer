9a9e5c62ad938f6a2bff9db03352436bc841fd33
Adenike Adeyemi <adenikeadeyemi@Adenike-Adeyemis-MacBook-Pro.local>
2016-08-16 23:08:26 -0400

    purged old code due to messy firebase code and reprogramed game for one round

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,247 +13,200 @@ $(document).ready(function(){
 	firebase.initializeApp(config);
 	
 	var database = firebase.database();
-	var connectedRef = database.ref('.info/connected');
-	var connectionsRef = database.ref('/connections');
-	var p1choice = "";
-	var p2choice = "";
-	var p1Stats = [0,0]; //win, lose
-	var p2Stats = [0,0];
-	var rps = ['rock', 'paper', 'scissors'];
-	var player1Exists = false;
-	var player2Exists = false;
-	var currentPlayer = 0;
-  	var playerCount = 0;
-  	var playerKey = null;
-  	var gameOn = false;
-  	//creating xor function
-  	function XOR(a,b){
-  		return (a && !b) || (b && !a)
-  	}
+	var playerNum = 0;
+	var clientNum = 0;
+	var checkPlayer1 = false;
+	var checkPlayer2 = false;
+	var gameOn = false;
+	var playersTurn = 0;
 
 
-  	//check player values---------------------------------------------------------------------------------------
-	database.ref('player').on("value", function(snapShot){
-		
-		console.log(snapShot.val());
+	database.ref().on("value", function(snap){
 
-		player1Exists = snapShot.child('1').exists();//checks player 1's existance
-		player2Exists = snapShot.child('2').exists();//checks player 2's existance
-
-
-		if(player1Exists){
-			console.log(snapShot.child('1').val().name);
-			$('.P1Name').text(snapShot.child('1').val().name);
-			$('#P1-Score').text('Wins: '+snapShot.child('1').val().win+' Loses: '+snapShot.child('1').val().lose);
-		}
-		else{
-			$('.P1Name').text('Waiting for Player 1');
-			$('#P1-Score').empty();
+		//checks if players exists
+		checkPlayer1 = snap.child('player/1').exists();
+		checkPlayer2 = snap.child('player/2').exists();
+		checkTurns = snap.child('turns').exists();
+		if(checkPlayer1 && checkPlayer2 && gameOn){
+			var p1chosen = snap.child('player/1').val().chosen;
+			var p2chosen = snap.child('player/2').val().chosen;
 		}
 
+		//update guest text
+		if(clientNum == 1)
+			$('#playerGreeting').text('Hi, '+snap.child('player/1').val().name+'! You are player 1');
+		else if(clientNum == 2)
+			$('#playerGreeting').text('Hi, '+snap.child('player/2').val().name+'! You are player 2');
+		else
+			$('#playerGreeting').text('Hi Guest!');
 
-
-		if(player2Exists){
-			console.log(snapShot.child('1').val().name);
-			$('.P2Name').text(snapShot.child('2').val().name);
-			$('#P2-Score').text('Wins: '+snapShot.child('2').val().win+' Loses: '+snapShot.child('2').val().lose);
+		//displays who's turn it is
+		if(checkPlayer1 && checkPlayer2 && checkTurns){
+			playersTurn = snap.child('turns').val();
+			if(clientNum == playersTurn){
+				$('#playerStatus').text("It's your turn");
+			}
+			else{
+				$('#playerStatus').text("Waiting for "+ snap.child('player/'+playersTurn).val().name +" to choose.")
+			}
 		}
-		else{
-			$('.P2Name').text('Waiting for Player 2');
-			$('#P2-Score').empty();
-		}
 
-		//keeps track of clients player number to set
-		if(player1Exists && !player2Exists){
-			playerCount = 2;
+		//display info in play area
+		if(checkPlayer1){
 			
+			$('#P1Name').text(snap.child('player/1').val().name);
+			if(!gameOn)clearChoices(1);
+			$('#P1-Score').text('Wins: '+snap.child('player/1').val().win+' Loses: '+snap.child('player/1').val().lose);
 		}
-		else if(!player1Exists){
-			playerCount = 1;
+		
+		if(checkPlayer2){
 			
+			$('#P2Name').text(snap.child('player/2').val().name);
+			if(!gameOn)clearChoices(2);
+			$('#P2-Score').text('Wins: '+snap.child('player/2').val().win+' Loses: '+snap.child('player/2').val().lose);
+		}
+		
+		//detrmines which player object will be made
+		if(!checkPlayer1){
+			playerNum = 1;
+		}
+		else if(!checkPlayer2){
+			playerNum = 2;
 		}
 
-		if(currentPlayer != 0 && !gameOn && player2Exists && player1Exists) setGame()
+		//only show when players are needed
+		if(!(checkPlayer1 && checkPlayer2)) $('#playerInput').show();
 
-		//deletes turn count when both players are not available at the same time
-  		if(!(player1Exists && player2Exists)){
-  			database.ref('turns').set(null);
-  		}
 
-  		console.log(currentPlayer);
+		//sets turn counter and choices
+		if(!gameOn && checkPlayer1 && checkPlayer2){
+			database.ref('turns').set(1);
 
-  		//allows watchers to join when seat is open
-		if(currentPlayer == 0 && !(player1Exists && player2Exists)) 
-			$('#playerInput').show();
-		
-		//---------------------------
-		
+			if(clientNum != 0){
+				displayChoices(clientNum);
+			}
+
+			gameOn = true;
+		}
+		//turns off turn counter
+		if(!(checkPlayer1 && checkPlayer2))
+		{
+			database.ref('turns').set(null);
+			gameOn = false;
+		}
+
+		//game
+		if(gameOn && p1chosen != undefined && p2chosen != undefined){
+			//stop firebase from looping
+			database.ref('player/1').update({chosen: null});
+			database.ref('player/2').update({chosen: null});
+
+			if(p1chosen == p2chosen){
+				console.log('tied');
+				p1chosen = undefined;
+				p2chosen = undefined;
+			}
+			else if((p1chosen == "rock" && p2chosen == "scissors") || 
+					(p1chosen == "paper" && p2chosen == "rock") || 
+					(p1chosen == "scissors" && p2chosen == "paper")){
+				//p1 wins
+				console.log('Player 1 wins');
+				p1chosen = undefined;
+				p2chosen = undefined;
+				console.log(snap.child('player/1').val().win);
+				var temp1 = snap.child('player/1').val().win;
+				var temp2 = snap.child('player/2').val().lose;
+				temp1++;
+				temp2++;
+				console.log(temp1);
+
+				database.ref('player/1').update({win: temp1});
+				database.ref('player/2').update({lose: temp2});
+
+				//database.ref('player/2').update({lose: snap.child('player/2').val().lose++}); 
+
+			}
+			else{
+				console.log('Player 2 wins');
+				p1chosen = undefined;
+				p2chosen = undefined;
+				console.log(snap.child('player/2').val().win);
+				var temp1 = snap.child('player/2').val().win;
+				var temp2 = snap.child('player/1').val().lose;
+				temp1++;
+				temp2++;
+				console.log(temp1);
+
+				database.ref('player/2').update({win: temp1});
+				database.ref('player/1').update({lose: temp2});
+				//database.ref('player/2').update({win: snap.child('player/2').val().win++}); 
+				//database.ref('player/1').update({lose: snap.child('player/1').val().lose++}); 
+			}
+		}
 
 	}, 
-	function(errorObj){console.log(errorObj.code)});
-	//--------------------------------------------------------------------------------
+		//catch any problems in firebase
+		function(errorObj){
+		console.log("Something went horibly wrong! Error Code: " + errorObj.code);
+	})
 
-
-	//set player
+	//submits player info
 	$('#nameSend').on('click', function(){
-		console.log(playerCount);
-		currentPlayer = playerCount;
-		console.log(currentPlayer);
-		var playerName = $('#playerName').val().trim();
+		clientNum = playerNum;
+		var newPlayer = database.ref('player').child(playerNum);
 
-		var players = database.ref().child('player')
-			
-		var con = players.child(playerCount);
-			
-		con.onDisconnect().remove();
-		con.set({
+		newPlayer.onDisconnect().remove();
 
-			name: playerName,
+		newPlayer.set({
+			name: $('#playerName').val().trim(),
 			win: 0,
-			lose: 0
+			lose:0
+		})
 
-		});
-
-		if(player1Exists && player2Exists){ 
-			database.ref('turns').set(1);
-			setGame();
-
-	}
-		
 		$('#playerInput').hide();
-		
 
-
-
-
-	});
-
-
-
-  	//get whats in chat input and print on text area
-	$('#chatSend').on('click', function(){
-
-		var c = $('#chatInput').val().trim();
-		var chat = database.ref().child('chat').push(c);
-		$('#chatInput').val('');
-		//$('#chatLog').text(c)
-
-		return false;
-	});
-
+	})
+  	
 	$('.P1choice').on('click', function(){
-
-		console.log(p1choice);
-		if(p1choice == ""){
-			p1choice = $(this).data('choice');
-			$('.P1choice').html('<br>');
-			$('.chosen1').text(p1choice);
+		if(1 == clientNum && clientNum == playersTurn){
+			database.ref('player/1').update({chosen: $(this).data('choice')});
+			displayChosen(1, $(this).data('choice'));
+			database.ref('turns').set(2);
 		}
-		if(p2choice != "") playGame();
-
-	});
+	})
 
 	$('.P2choice').on('click', function(){
-
-		console.log(p2choice);
-		if(p2choice == ""){
-			p2choice = $(this).data('choice');
-			$('.P2choice').html('<br>');
-			$('.chosen2').text(p2choice);
+		if(2 == clientNum && clientNum == playersTurn){
+			database.ref('player/2').update({chosen: $(this).data('choice')});
+			displayChosen(2, $(this).data('choice'));
 		}
-		if(p1choice != "") playGame();
-
-	});
-
-	function setP1Game(){
-		
-		for(var i = 0; i < rps.length; i++){
-			$('#P1-'+(i+1)).text($('#P1-'+(i+1)).data('choice'));
-			/*$('<p>')
-				.attr('id', 'P1-'+i)
-				.addClass('choice')
-				.data('choice', rps[i])
-				.text(rps[i])
-				.appendTo($('#player1'));*/
-
-		}
-		
-		//$('#P1-Score')
-		//	.html('Wins: <span class="P1Wins">'+p1Stats[0]+'</span> Loses: <span class="P1Loses">'+p1Stats[1]+'</span>')
-			
-		
-	}
-
-	function setP2Game(){
-		
-		for(var i = 0; i < rps.length; i++){
-			$('#P2-'+(i+1)).text($('#P1-'+(i+1)).data('choice'));
-			
-			/*$('<p>')
-				.attr('id', 'P1-'+i)
-				.addClass('choice')
-				.data('choice', rps[i])
-				.text(rps[i])
-				.appendTo($('#player1'));*/
-
-		}
-		//$('#P2-Score')
-		//	.html('Wins: <span class="P2Wins">'+p2Stats[0]+'</span> Loses: <span class="P2Loses">'+p2Stats[1]+'</span>')
-		
-	}
-
-	function setGame(){
-
-		if(currentPlayer != 0){
-
-			(currentPlayer == 1) ? setP1Game() : setP2Game();
-		}
-	}
-    //setP1Game();
-    //setP2Game();
-    
-    //run the comparison game
-    function playGame(){
-    	if(p1choice == p2choice){
-    		$('#results p').text("TIED");
-    		
-    	}
-    	else if( (p1choice == 'rock' && p2choice == 'scissors' )||
-    		(p1choice == 'scissors' && p2choice == 'paper' )||
-    		(p1choice == 'paper' && p2choice == 'rock')){
-    		$('#results p').text("Player 1 wins");
-    		p1Stats[0]++;
-    		p2Stats[1]++;
-
-    		$('#P1-Score')
-			.html('Wins: <span class="P1Wins">'+p1Stats[0]+'</span> Loses: <span class="P1Loses">'+p1Stats[1]+'</span>')
-
-			$('#P2-Score')
-			.html('Wins: <span class="P2Wins">'+p2Stats[0]+'</span> Loses: <span class="P2Loses">'+p2Stats[1]+'</span>')
+	})
 
 
-    	}
-    	else{
-    		$('#results p').text("Player 2 wins");
-    		p1Stats[1]++;
-    		p2Stats[0]++;
 
-    		$('#P1-Score')
-			.html('Wins: <span class="P1Wins">'+p1Stats[0]+'</span> Loses: <span class="P1Loses">'+p1Stats[1]+'</span>')
+  	//clears out choices
+  	function clearChoices(player){
+  		for(var i = 1; i <= 3; i++){
+  			$('#P'+player+'-'+i).html('<br>');
+  		}
+  	}
 
-			$('#P2-Score')
-			.html('Wins: <span class="P2Wins">'+p2Stats[0]+'</span> Loses: <span class="P2Loses">'+p2Stats[1]+'</span>')
+  	//displays all choices
+  	function displayChoices(player){
+  		for(var i = 1; i <= 3; i++){
+  			$('#P'+player+'-'+i).text($('#P'+player+'-'+i).data('choice'));
+  		}
+  	}
 
-    	}
-    	
-    	p1choice = "";
-    	p2choice = "";
+  	//display player's choice
+  	function displayChosen(player, choice){
 
-    	setTimeout(function(){
-    		setP1Game();
-    		setP2Game();
+  		clearChoices(player);
+  		$('.chosen'+player).text(choice);
+  	}
+  	
+  	function playGame(){
 
-    	}, 3000);
-    }
+  	}
 
 });
